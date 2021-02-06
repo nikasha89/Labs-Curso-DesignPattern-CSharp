5c738b492e7ae85bc589940d234384e9bb56a6af
AnaRodriguez <nikasha_moi@hotmail.com>
2021-02-06 16:42:06 +0100

    My Solution: Single Responsability Principle using TDD.

---
+++ 1-SRP/2 Exercices/MySolution/MySolution/MySolution/Program.cs
@@ -0,0 +1,19 @@
+﻿using Problem.BusinessClasses;
+using System;
+
+namespace Problem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var report = new Report();
+            report.AddCourse(new Course ("Patrones de diseño", 1000, 5));
+            report.AddCourse(new Course ("Flutter", 1900, 4.5));
+
+            Console.WriteLine(report.ToString());
+
+            report.SaveToFile(@"Reports", "WorkReport.txt");
+        }
+    }
+}
---
+++ 1-SRP/2 Exercices/MySolution/MySolution/MySolutionTest/BusinessClasses/ReportTest.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySolution.BusinessClasses;
+using System.Collections.Generic;
+using System.IO;
+
+namespace MySolutionTest
+{
+    [TestClass]
+    public class ReportTest
+    {
+        private Report report;
+        private List<Course> courses;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            courses = new List<Course> {
+                new Course("Patrones de diseño", 1000, 5),
+                new Course("Flutter", 1900, 4.5)
+            };            
+        }
+
+        [TestMethod]
+        public void TestAddCourse_TwoCourses_2CoursesAddedToReportList()
+        {
+            report = new Report();
+
+            Assert.AreEqual(0, report.Courses.Count);
+            
+            foreach (var course in courses)
+                report.AddCourse(course);
+
+            Assert.AreEqual(2, report.Courses.Count);
+        }
+
+        [TestMethod]
+        public void TestRemoveCourseAt_TwoCourses_1CourseRemovedFromReportList()
+        {
+            report = new Report(courses);
+            
+            Assert.AreEqual(2, report.Courses.Count);
+
+            report.RemoveCourseAt(0);
+
+            Assert.AreEqual(1, report.Courses.Count);
+        }
+
+        [TestMethod]
+        public void TestSaveToFile_TwoCourses_2FilesSaved()
+        {
+            report = new Report(courses);
+
+            report.SaveToFile("Report","LaPaca");
+
+            Assert.IsTrue(File.Exists(Path.Combine("Report", "LaPaca")));
+        }
+    }
+}
