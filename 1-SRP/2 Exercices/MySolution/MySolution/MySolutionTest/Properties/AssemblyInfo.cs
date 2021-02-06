8a68d4c93e3b730a5d176a0bfbc525834d8a80f3
AnaRodriguez <nikasha_moi@hotmail.com>
2021-02-06 16:42:06 +0100

    My Solution: Using Single Responsability Principle using TDD.

---
+++ 1-SRP/2 Exercices/MySolution/MySolution/MySolutionTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MySolutionTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MySolutionTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e75468c0-f621-497a-80e3-fe7927983006")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
