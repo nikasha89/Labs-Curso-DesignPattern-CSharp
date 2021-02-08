1c234c702dff7211b4b0c20e34290c0d85d41096
AnaRodriguez <nikasha_moi@hotmail.com>
2021-02-08 03:36:46 +0100

    Builder and Fluent Builder Patterns

---
+++ 6.2-Fluent Builder/Exercise/FluentBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using BuilderLibrary;
+
+namespace FluentBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new CarBuilder();
+        }
+    }
+}
---
+++ 6.2-Fluent Builder/MySolution/FluentBuilder/Program.cs
@@ -0,0 +1,18 @@
+﻿using BuilderLibrary;
+using System;
+
+namespace FluentBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new CarBuilder()
+                .WithModel("Mercedes")
+                .WithName("Benz");
+
+            Console.WriteLine(builder.Car.Name + " " + builder.Car.Model);
+            Console.ReadLine();
+        }
+    }
+}
